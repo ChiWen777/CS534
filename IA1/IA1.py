e6e474f4377861e6055ac36e88d0a788d3526da7
ChiWen777 <wench@oregenstate.edu>
2018-10-10 14:48:18 -0700

    Chi update - norm

--- IA1/IA1.py
+++ IA1/IA1.py
@@ -5,6 +5,87 @@ import os
 
 train = pd.read_csv('PA1_train.csv', sep=',',header=None)
 train = train.as_matrix()
+test = pd.read_csv('PA1_test.csv', sep=',',header=None)
+test = test.as_matrix()
+
+
+def split_date(cut_head_data):
+    r = 0
+
+    split_date_data = copy.deepcopy(cut_head_data)
+
+    sd_data = np.zeros((10000,3))
+        
+    for ea_date_str in split_date_data:
+        data_features = ea_date_str.split("/")
+        for idx in range(0,3):
+            sd_data[r,idx] = data_features[idx-1]
+        r += 1
+    h_data_set = np.hsplit(sd_data,3)
+    return h_data_set
+
+def add_in_arrays(ea_col, data, min_array, max_array):
+    max_array[ea_col] = np.max(data)
+    min_array[ea_col] = np.min(data)
+
+def norm_data(ea_col, cut_head_data, min_array, max_array):
+    new_data = (cut_head_data - min_array[ea_col]) / (max_array[ea_col] - min_array[ea_col])
+    print(new_data)
+    print('=====')
+#     normalized_data[:, ea_col] = new_data
+
+def process_columns():
+    """
+    Process both test.csv and train.csv 's columns and normalize them
+    """
+
+    # Run through every col in train.csv
+    normalized_data = np.zeros((10000, 23))
+    min_array = np.zeros((train.shape[1]+1,))
+    max_array = np.zeros((train.shape[1]+1,))
+    # print(mean_array.shape)
+
+    for ea_col in range(train.shape[1]-1):
+        orig_data = train[:,ea_col]
+        
+        cut_head_data = copy.deepcopy(orig_data)
+        cut_head_data = cut_head_data[1:]
+        if ea_col == 2:
+            date_data = split_date(cut_head_data)
+            for ea_date_data in date_data:
+                add_in_arrays(ea_col, ea_date_data, min_array, max_array)
+                norm_data(ea_col, ea_date_data, min_array, max_array)
+        elif ea_col == 0:
+            pass
+        else:
+            cut_head_data = cut_head_data.astype(float)
+            add_in_arrays(ea_col, cut_head_data, min_array, max_array)
+            norm_data(ea_col, cut_head_data, min_array, max_array)
+
+    ##########################################################################
+
+    # Run through every col in test.csv
+    normalized_data = np.zeros((10000, 23))
+    min_array = np.zeros((test.shape[1]+2,))
+    max_array = np.zeros((test.shape[1]+2,))
+
+    for ea_col in range(test.shape[1]):
+        orig_data = test[:,ea_col]
+        
+        cut_head_data = copy.deepcopy(orig_data)
+        cut_head_data = cut_head_data[1:]
+        if ea_col == 2:
+            date_data = split_date(cut_head_data)
+            for ea_date_data in date_data:
+                add_in_arrays(ea_col, ea_date_data, min_array, max_array)
+                norm_data(ea_col, ea_date_data, min_array, max_array)
+        elif ea_col == 0:
+            pass
+        else:
+            cut_head_data = cut_head_data.astype(float)
+            add_in_arrays(ea_col, cut_head_data, min_array, max_array)
+            norm_data(ea_col, cut_head_data, min_array, max_array)
+
 
 
 """
