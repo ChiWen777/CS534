d5e1acce468887271e376882d5aca4c0f1ef2d80
函諭 <bill@hanyude-MacBook-Pro.local>
2018-11-24 23:05:54 -0800

    bill update

--- IA3/IA3.py
+++ IA3/IA3.py
@@ -4,6 +4,8 @@ from math import log
 import csv
 import math
 import operator
+from collections import OrderedDict
+import time
 
 
 ##########Read File###########
@@ -71,42 +73,23 @@ def x_feature(x):
 	return x_T
 
 ############Split data###################
-def split_data(y_index, t, t_index):
-	'''
-	y_index: list
-	t : int (theta)
-	t_index: [x, y]
-	y: original y
-	'''
+def split_data(data_left, data_right):
+ '''
+ data_left, data_right : [index, y, x1....,xn]
+ '''
+ left_y, right_y = [], []
 
-	a = x_feature(x_array_train) 
-	y = y_array_train
-	x = a[t_index[1]]					#which column
-	x_value = x[y_index]				#x's value(which row)
-	y_value = y[y_index]				#y's value(which row)
+ for i in range(0, len(data_left)):
+  left_y.append(data_left[i][1][0])
 
+ for i in range(0, len(data_right)):
+  right_y.append(data_right[i][1][0])
 
-	right_index, left_index = [], []
-	left_y, right_y = {}, {}
-
-####split data######
-	for v in range(0, len(y_index)):
-		if x_value[v] >= t:
-			right_index.append(y_index[v])
-			right_y[v] = y_value[v]
-		else:
-			left_index.append(y_index[v])
-			left_y[v] = y_value[v]
-
-
-	right_y_value = right_y.values()
-	left_y_value = left_y.values()
-	count_right_pos = list(right_y_value).count(1)
-	count_right_neg = list(right_y_value).count(-1)
-	count_left_pos = list(left_y_value).count(1)
-	count_left_neg = list(left_y_value).count(-1)
-	return count_left_neg,count_left_pos,count_right_neg, count_right_pos, right_index, left_index
-
+ count_right_pos = list(right_y).count(1)
+ count_right_neg = list(right_y).count(-1)
+ count_left_pos = list(left_y).count(1)
+ count_left_neg = list(left_y).count(-1)
+ return count_left_neg,count_left_pos,count_right_neg, count_right_pos
 
 
 # ############Root U Value######################
@@ -116,44 +99,52 @@ def U_value(neg,pos):
 	return u 
 
 
-def B_value(theda, left_neg,left_pos, right_pos, right_neg, U_root):
+def B_value(left_neg,left_pos, right_pos, right_neg, U_root):
 	# right_pos, right_neg, legt_pos, left_neg = count_neg_pos(theda, y_array, x_array)
 	pb_l= (left_pos+left_neg)/(right_pos+right_neg+left_pos+left_neg)
 	pb_r= (right_pos+right_neg)/(right_pos+right_neg+left_pos+left_neg)
 	B_value = U_root - pb_l*U_value(left_neg,left_pos) - pb_r*U_value(right_neg,right_pos)
 	return B_value
 
-def best_B(index,x_array, neg, pos):
+def best_B(x_array, neg, pos):
 	U_root = U_value(neg, pos)
-	# print(U_root)
 	best_b = 0
-	index_pos = []
-	index_neg = []
+	temp_theda_index = [0,0]
 	theda_index= [0,0]
-	# a = index
-	# b = 
-	# c = 
-	for i in index:  
-		for j in range(0,100):
-			theda = x_array[i][j]
-			# print(x_array[i][j])
-			temp_left_neg, temp_left_pos, temp_right_neg, temp_right_pos, temp_index_right, temp_index_left = split_data(index ,theda ,[i,j])
-			# print(temp_left_neg, temp_left_pos, temp_right_neg, temp_right_pos, temp_index_right, temp_index_left)
-			if temp_left_neg==temp_left_pos==0 or temp_right_pos==temp_right_neg==0:
-				temp_b=0
-			else:
-				temp_b = B_value(theda,temp_left_neg, temp_left_pos, temp_right_pos, temp_right_neg, U_root )
-			if temp_b > best_b:
-				theda = x_array[i][j]
-				left_pos = temp_left_pos
-				left_neg = temp_left_neg
-				right_neg = temp_right_neg
-				right_pos = temp_right_pos
-				best_b = temp_b
-				index_left = temp_index_left
-				index_right = temp_index_right
-	# print(theda, left_neg, left_pos, right_neg, right_pos, index_left, index_right)
-	return theda, left_neg, left_pos, right_neg, right_pos, index_left, index_right
+	pre_y_value=0
+	curr_y_value=-1
+	count = 0
+	for y in range(1,101):
+		print("looking feaure",y)
+		time_a = time.clock()
+		x_array_sorted=sorted(x_array, key= lambda x: x[1][y])
+		for i in range(1,(len(x_array))):
+			pre_y_value = curr_y_value
+			curr_y_value = x_array_sorted[i][1][0]
+			if pre_y_value != curr_y_value:
+				count += 1
+				theda = x_array_sorted[0][1][y]
+				temp_left_neg, temp_left_pos, temp_right_neg, temp_right_pos = split_data(x_array_sorted[0:i-1],x_array_sorted[i:-1])
+				if temp_left_neg==temp_left_pos==0 or temp_right_pos==temp_right_neg==0:
+					temp_b=0
+				else:
+					temp_b = B_value(temp_left_neg, temp_left_pos, temp_right_pos, temp_right_neg, U_root )
+				if temp_b > best_b:
+					theda = x_array_sorted[0][1][y]
+					left_pos = temp_left_pos
+					left_neg = temp_left_neg
+					right_neg = temp_right_neg
+					right_pos = temp_right_pos
+					left_array = x_array_sorted[0:i-1]
+					right_array = x_array_sorted[i:-1]
+					best_b = temp_b
+		print ("computation count in feature:",count)
+		count = 0
+		time_b = time.clock()
+		print(best_b)
+		print(time_b-time_a," sec")
+
+	return theda, left_neg, left_pos, right_neg, right_pos, left_array, right_array
 
 
 
@@ -241,9 +232,29 @@ y_array_valid = y_data('pa3_valid_reduced.csv')
 x_array_valid = x_data('pa3_valid_reduced.csv')
 
 
-dic = list(range(0, 4888))
+# dic = list(range(0, 4888))
+# dic_data = dict(zip(dic, total_train))
+# values = list(dic_data.values())
+# print(values[0][1])
 
-dic_data = dict(zip(dic, total_train))
-values = list(dic_data.values())
-print(values[0][1])
+length = list(range(len(x_array_train)))
+# print(total_train)
+dic_data = list(zip(length, total_train))
 
+# d_sorted_by_value =sorted(dic_data, key= lambda x: x[1][3])
+# print(d_sorted_by_value)
+root_pos = list(y_array_train).count(1)
+root_neg = list(y_array_train).count(-1)
+
+
+print(best_B(dic_data,root_pos,root_neg))
+
+# print(dic_data)
+##############data for sorted#################
+# values.sort(key=operator.itemgetter(1))\
+# d_sorted_by_value =sorted(dic_data.items(), key= lambda x: x[1][3])
+# dic.sort(key=operator.itemgetter(1))
+# print(values)
+# print(d_sorted_by_value)
+# for key,value in sorted(dic_data.items(), key= lambda x: x[1][3]):
+# 	print (key)
